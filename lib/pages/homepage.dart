bf9c56cb931319da144564bd274648781d312577
anjesh-shni <anjeshshni1@gmail.com>
2022-09-28 13:32:56 +0545

    new changes

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:movie_app/reusable_widget/big_text.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 import '../constant/dynamic_dimension.dart';
 import 'main_page.dart';
 
@@ -12,66 +13,144 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> _data=[];
+  List<String> _filterData=[];
+  final _searchQuery=new TextEditingController();
+  Timer? _debounce;
+  String searchText='';
+
+
+
+  @override
+  void initState(){
+    super.initState();
+    _data.add("Jankpur");
+    _data.add("Kathmandu");
+    _data.add("Biratnagar");
+    _data.add("Lahan");
+    _data.add("chabhil");
+    _data.add("Dhungedhara");
+    _data.add("Enaruwa");
+    _data.add("Ithari");
+    _data.add("Gongbu");
+    _data.add("Hatiban");
+    _data.add("jorpati");
+    _data.add("kumripTi");
+    _data.add("lalitpur");
+    _data.add("lubu");
+    _data.add("Manthali");
+    _data.add("nakhu");
+    _data.add("orisas");
+
+    _filterData=this._data;
+  
+    _searchQuery.addListener(() {
+      _onSearchChanged();
+    });
+  }
+
+
+  
+  _onSearchChanged(){
+    if(_debounce?.isActive??false) _debounce?.cancel();
+    _debounce=Timer(const Duration(microseconds: 100),
+    (){
+      if(this.searchText!=_searchQuery.text){
+        this._filterData=this._data;
+        setState(() {
+          this._filterData=this._filterData
+          .where((element) =>
+           element.toString().toLowerCase()
+          .contains(_searchQuery.text.toString().toLowerCase()),).toList();
+        });
+      }
+      this.searchText=_searchQuery.text;
+     }
+    );
+  }
+  @override
+  void dispose(){
+    _searchQuery.removeListener(_onSearchChanged());
+    _searchQuery.dispose();
+    _debounce?.cancel();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black54,
      
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const SizedBox(height: 10,),
-         Container(
-          margin: const EdgeInsets.only(left: 20,right: 20,top: 40),
-          child:Center(
-            child: BigText(text: "Search for movie",
-            color: Colors.white,
-            size: 16,
-            ),
-           ),
-          ),
           const SizedBox(height: 20,),
-
           //Search field
-           Container(
-            margin: const EdgeInsets.only(left: 30,right: 30),
-             child: TextField( 
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey,
-                border:OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide.none,
-                ),
-                hintText: "Type something",
-                hintStyle:  TextStyle(color: Colors.grey[700]),
-                prefixIcon:  Icon(Icons.search,color: Colors.grey.shade600,),
-                suffixIcon: const Icon(Icons.clear,color: Colors.black,)
-              ),
-              style:const TextStyle(color: Colors.white),
-             ),
+           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+             children: [
+               Container(
+                height: 50,
+                width: 350,
+                margin: const EdgeInsets.only(left: 20,right:10,),
+                 child: TextField( 
+                  controller: _searchQuery,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey,
+                    focusedBorder:  OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade800,
+                        width: 1,
+                        ),
+                      ),
+                      enabledBorder:const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1,
+                        ),
+                      ),
+                    border:OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    hintText: "search here",
+                    hintStyle:  TextStyle(color: Colors.grey[700]),
+                    prefixIcon:  Icon(Icons.search,color: Colors.grey.shade700,),
+                    suffixIcon: const Icon(Icons.clear,color: Colors.black,)
+                  ),
+                  style:const TextStyle(color: Colors.white),
+                 ),
+               ),
+               Container(
+                margin:const EdgeInsets.only(right: 20,),
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.grey
+                  ),
+                  child: const Icon(Icons.search,color: Colors.black,),
+               ),
+             ],
            ),
-          //  Container(
-          //   height: 45,
-          //   width: 45,
-          //   decoration: BoxDecoration(
-          //   borderRadius: BorderRadius.circular(10),
-          //   color: Colors.grey
-          //   ),
-          //   child:const Center(
-          //     child: Icon(
-          //       Icons.search,
-          //     color: Colors.black,)),
-          //   ),
-
+           
           // body section where movie list shows
           SizedBox(height: Dimen.font20),
-          const Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: MainPage(),
-            ),
+           Expanded(
+              child:ListView.builder(
+                itemCount: _filterData.length,
+                itemBuilder:(context,index){
+                    return Padding(
+                      padding: const EdgeInsets.only(left:30.0),
+                      child: ListTile(
+                        title: Text(_filterData[index].toString()),
+                      ),
+                    );
+                  })
+            // child: SingleChildScrollView(
+            //   child: MainPage(),
+            // ),
           ),
         ],
       ),
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -13,36 +13,45 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        GridView.builder(
-         gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
-           crossAxisCount: 2,childAspectRatio: 1,
-         ), 
-         itemCount: 20,
-         itemBuilder: (context,index){
-           return Container(
-             decoration: BoxDecoration(
-               borderRadius: BorderRadius.circular(10),
-               color: Colors.white,
-               boxShadow: const [
-                 BoxShadow(
-                   color: Colors.black,
-                   blurRadius: 5.0,
-                 ), 
-                 BoxShadow(
-                   color: Colors.green,
-                   offset: Offset(-5, 0),
+        Container(
+            margin:  const EdgeInsets.all(30),
+            padding: const EdgeInsets.all(30),
+            width: MediaQuery.of(context).size.width,
+            height: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage("https://images.unsplash.com/photo-1664154026818-b49c159c91d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60"),),
+              color: Colors.grey,
+            ),
+            child:const Center(child: Text("Try premium"),),
+           ),
+        Container(
+          padding: const EdgeInsets.all(10),
+          height: MediaQuery.of(context).size.height,
+          child: GridView.builder(
+           gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+             crossAxisCount: 2,childAspectRatio: 1.3,
+           ), 
+           itemCount: 20,
+           itemBuilder: (context,index){
+             return Container(
+               margin: const EdgeInsets.all(10),
+               decoration: BoxDecoration(
+                 borderRadius: BorderRadius.circular(10),
+                 color: Colors.white,
+                 image:const DecorationImage(
+                  image: NetworkImage("https://images.unsplash.com/photo-1664073412845-f44940c3c2c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60"),
+                  fit: BoxFit.cover
+                  ),                   
                  ),
-                 BoxShadow(
-                 color: Colors.green,
-                 offset: Offset(5, 0),
-                 ),
-               ],
-             ),
-             child: const Center(
-               child: Text("Movie"),
-             ),
-           );
-         }
+               child: const Center(
+                 child: Text("Movie"),
+               ),
+             );
+           }
+          ),
         ),
       ],
     );
